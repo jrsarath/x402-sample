f8d76721b0c092e7090bf4e6e0777ed4b801cd3d
Sarath "Delta" Singh <jrsarath@outlook.com>
2025-06-02 16:00:50 +0530

    rebase

---
+++ index.ts
@@ -0,0 +1,47 @@
+import axios from "axios";
+import { config } from "dotenv";
+import { Hex } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+import { withPaymentInterceptor, decodeXPaymentResponse } from "x402-axios";
+
+config();
+
+const privateKey = process.env.PRIVATE_KEY as Hex;
+const baseURL = process.env.RESOURCE_SERVER_URL as string; // e.g. https://example.com
+const endpointPath = process.env.ENDPOINT_PATH as string; // e.g. /weather
+
+if (!baseURL || !privateKey || !endpointPath) {
+  console.error("Missing required environment variables");
+  process.exit(1);
+}
+
+const account = privateKeyToAccount(privateKey);
+
+const api = withPaymentInterceptor(
+  axios.create({
+    baseURL,
+  }),
+  account,
+);
+
+api
+  .post(endpointPath, { 
+    "amount": 1,
+    "currency": "USDC",
+    "type": "social-network",
+    "sender_username": "iamdelta48",
+    "sender_name": "Sarath Singh",
+    "sender_identity": "twitter",
+    "receiver_username": "jrsarath48",
+    "receiver_name": "Sarath Singh",
+    "receiver_identity": "twitter",
+   })
+  .then(response => {
+    console.log(response.data);
+
+    const paymentResponse = decodeXPaymentResponse(response.headers["x-payment-response"]);
+    console.log(paymentResponse);
+  })
+  .catch(error => {
+    console.error(error.response?.data);
+  });
